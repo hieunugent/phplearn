5159ed2bd89b4c8cebf097d7a2fa39ca25255fc7
hieunugent <hieunugent@gmail.com>
2021-09-11 15:53:56 -0700

    update index

--- index.php
+++ index.php
@@ -11,6 +11,37 @@ for ($i= 0; $i < count($names);$i++){
     echo "Hello $names[$i] <br>";
     echo "My Age is $age[$i] <br>";
 }
+function add($q, $p){
+    $value = $q + $p;
+    echo "<p> Value of adding $q and $p is : $value </p>";
+}
+ add(1, 3);
+ $a = 15;
+ $b = 10;
+function multi(){
+    $GLOBALS['a'] = $GLOBALS['a']+ $GLOBALS['b'];
+}
+
+multi();
+echo "$a <br>";
+class person{
+    public $name;
+    public $year;
+    public $job;
+    public function __construct($name, $year){
+        $this->name = $name;
+        $this->age = date("Y") - $year ;
+    }
+    public function message(){
+        return "Your name is " .$this->name . " and your age is " .$this->age."!";
+
+    }
+}
+$newperson = new person("Henry", 1988);
+echo $newperson->message();
+echo "<br>";
+
+
 
 ?>
 
