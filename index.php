684f7c1f14258612bdc253a45c58ee9ca3045fe4
hieunugent <hieunugent@gmail.com>
2021-09-15 23:13:55 -0700

    update

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?php
+$cookie_name = "user";
+$cookie_value = "Alex Porter";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+?>
 <html>
 <body> 
 
@@ -192,6 +197,16 @@ fclose($myfile);
     }
 ?>
 
+<!-- set cookie -->
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+  echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+  echo "Cookie '" . $cookie_name . "' is set!<br>";
+  echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
 
 
 </body>
