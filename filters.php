fc61cbfa3ebc25e70e0ccc613578363ec421fb35
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-09-16 18:47:30 -0700

    update learning

---
+++ filters.php
@@ -0,0 +1,15 @@
+<?php
+$str = "<h1>Hello World!</h1>";
+$newstr = filter_var($str, FILTER_SANITIZE_STRING);
+echo $newstr;
+?>
+
+<?php
+$int = 0;
+
+if (filter_var($int, FILTER_VALIDATE_INT) ===0 ||!filter_var($int, FILTER_VALIDATE_INT) === false) {
+  echo("Integer is valid");
+} else {
+  echo("Integer is not valid");
+}
+?>
