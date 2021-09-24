86c59eb564b059fa5f993ac27d31b6f7639b9e2c
hieunugent <hieunugent@gmail.com>
2021-09-24 16:04:10 -0700

    update\

--- Blog/main.php
+++ Blog/main.php
@@ -6,19 +6,7 @@
     </head>
    <body>
        <div>
-           <form action="post.php"  method="post" enctype="multipart/form-data">
-                <h3>Title</h3>
-               <input type="text" name="title">
-               <br>
-               <h3>Post Paragraph</h3>
-               <textarea type="text" name="contents" id="" cols="50vw" rows="10"></textarea>
-               <br>
-            Select image to upload:
-               <input type="file" name="fileToUpload" id="fileToUpload">
-
-            <br>
-               <input type="submit" >
-           </form>
+           <button>add post</button>
        </div>
    </body>
 </html>
--- Blog/post.php
+++ Blog/post.php
@@ -64,3 +64,27 @@ if ($uploadOk == 0) {
   }
 }
 ?>
+
+<html>
+    <head>
+    <script>
+
+    </script>
+    </head>
+   <body>
+       <div>
+           <form action="post.php"  method="post" enctype="multipart/form-data">
+                <h3>Title</h3>
+               <input type="text" name="title">
+               <br>
+               <h3>Post Paragraph</h3>
+               <textarea type="text" name="contents" id="" cols="50vw" rows="10"></textarea>
+               <br>
+            Select image to upload:
+               <input type="file" name="fileToUpload" id="fileToUpload">
+            <br>
+               <input type="submit" >
+           </form>
+       </div>
+   </body>
+</html>
