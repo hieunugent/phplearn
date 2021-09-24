beb747bfb6b81baad8f9510d93086fc2f34e9a19
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-09-24 10:42:29 -0700

    update:

---
+++ Blog/main.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+    <script>
+
+    </script>
+    </head>
+   <body>
+       <div>
+           <form action="sigalPost.php">
+                <h3>Title</h3>
+               <input type="text" name="title">
+               <br>
+               <h3>Post Paragraph</h3>
+               <textarea name="postitem" id="" cols="50vw" rows="10"></textarea>
+               <br>
+            <h3> Upload picture</h3>
+            <input type="file" name="filetoupload" id="filetoupload">
+            <br>
+               <input type="submit">
+           </form>
+       </div>
+   </body>
+</html>
