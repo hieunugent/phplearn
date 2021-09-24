916b01d079fd9e97fdd7bd41513e9e7093272b67
hieunugent <hieunugent@gmail.com>
2021-09-24 14:20:18 -0700

    update

--- Blog/main.php
+++ Blog/main.php
@@ -6,17 +6,18 @@
     </head>
    <body>
        <div>
-           <form action="sigalPost.php">
+           <form action="post.php"  method="post" enctype="multipart/form-data">
                 <h3>Title</h3>
                <input type="text" name="title">
                <br>
                <h3>Post Paragraph</h3>
-               <textarea name="postitem" id="" cols="50vw" rows="10"></textarea>
+               <textarea type="text" name="contents" id="" cols="50vw" rows="10"></textarea>
                <br>
-            <h3> Upload picture</h3>
-            <input type="file" name="filetoupload" id="filetoupload">
+            Select image to upload:
+               <input type="file" name="fileToUpload" id="fileToUpload">
+
             <br>
-               <input type="submit">
+               <input type="submit" >
            </form>
        </div>
    </body>
