846ba7908be2aa36dc3a0ada759b8ad28786bdb4
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-09-25 16:42:06 -0700

    update

--- Blog/mongo.php
+++ Blog/mongo.php
@@ -1,9 +1,7 @@
 <?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-$client = new MongoDB\Client(
-      'mongodb+srv://<username><password>@myfirstcluster.zbcul.mongodb.net/dbname?retryWrites=true&w=majority');
-$customers = $client->selectCollection('sample_analytics', 'customers');
-$document = $customers->findOne(['username' => 'wesley20']);
-var_dump($document);
+require __DIR__ .'/vendor/autoload.php';
+$client = new MongoDB\Client("mongodb://localhost:27017");
+$collection = $client->mongophp->details;
+$result = $collection->insertOne(['newtext'=>"currentText"]);
+echo " done show insert Data";
 ?>
