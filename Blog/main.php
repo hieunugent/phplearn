c39bffcca0f3cced5c1b8b55fcd75e1253c86f2b
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-10-04 13:30:59 -0700

    update CSS

--- Blog/main.php
+++ Blog/main.php
@@ -5,7 +5,9 @@
 <html>
     <head>
     <script>
-    
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+     }
     
     </script>
     <style>
@@ -19,19 +21,18 @@
       }
     
     </style>
-     <link rel="stylesheet" href="views/post.css">
+     <link rel="stylesheet" href="views/main.css">
     </head>
    <body class="page">
-       <div class="navbar_section">
-         <div class="navTilte">
-             <h3 >Home Page</h3>
-         </div>
-         <div class="navLogo" >
-         <img class="logoimage" src="/uploads/blog.jpg" alt="logo">
-         </div>
-        
-         <a class="navBtnA" href="post.php" >ADD</a>
-         
+       <div class="main_navbar_section">
+              <div class="main_navTilte">
+                  <h3 >My Blog</h3>
+              </div>
+              <div class="main_navLogo" >
+                    <img class="logoimage" src="/uploads/blog.jpg" alt="logo">
+              </div>
+              
+                  <a class="main_navBtnA" href="post.php" >ADD</a>    
        </div>
        <div class="mainpage">
        <?php
@@ -45,40 +46,41 @@
        }
       
        foreach ($cursor as $obj){ ?>
-        <div class="displayItem">
+        <div class="main_displayItem">
         <!-- <h3><?php echo $obj["_id"] ?></h3> -->
-        <h3 class="titleOutput"><?php echo $obj["title"] ?></h3>
-        <div>
-         
-        <p class="journalOutput">  
-           <?php
-            if ($obj["cover"]!=""){ ?>
-             <img class="postimage" src="data:jpeg;base64,<?=base64_encode($obj->cover->getData())?>"alt="postimage" >
-           <?php }
-           else{ ?>
-              <img class="postimage" src="https://picsum.photos/200/300" alt="postimage" >
-          <?php  } 
-           ?>    
-           <?php echo $obj["journal"]?>
-          
-          </p>
-       </div> 
-       <div class="dropdown">
-         <button onclick="myFunction()"> ○ ○ ○ </button>
-            <div class="button_group">
-              <a class="myBtn" href="update.php?varUpdate=<?php echo $obj["_id"]?>"><button class="myBtn">Edit </button></a>
-              <a class="myBtn" href="main.php?vardelete=<?php echo $obj["_id"] ?>"><button class="myBtn">Delete</button></a>
-              <a class="myBtn" href="view.php?valueId=<?php echo $obj["_id"] ?>"> <button class="myBtn">View</button></a>
+            <div class="main_topdisplayItem">
+                  <h3 class="main_titleOutput"><?php echo $obj["title"] ?></h3>
+                  <div class="main_dropdown">
+                      <button onclick="myFunction()" class='main_dropBtn'> ⋮ </button>
+                      <div id="myDropdown" class="main_dropdown-content">
+                            <a  href="update.php?varUpdate=<?php echo $obj["_id"]?>">Edit </a>
+                            <a  href="main.php?vardelete=<?php echo $obj["_id"] ?>">Delete</a>
+                            <a  href="view.php?valueId=<?php echo $obj["_id"] ?>"> View</a>
+                      </div>
+                 </div>
             </div>
-       </div>
+           
+            <div class="main_journalOutput">   
+                <p >  
+                      
+                  <?php echo $obj["journal"]?>
+                  
+                </p>
+                <?php
+                    if ($obj["cover"]!=""){ ?>
+                    <img class="main_postimage" src="data:jpeg;base64,<?=base64_encode($obj->cover->getData())?>"alt="postimage" >
+                  <?php }
+                  else{ ?>
+                      <img class="main_postimage" src="https://picsum.photos/200/300" alt="postimage" >
+                      <?php  } 
+                  ?>    
+             </div> 
+      
         
         </div>
-       <?php
-
-      
-      }?>
+          <?php }?>
      
 
-         </div>
+      </div>
    </body>
 </html>
