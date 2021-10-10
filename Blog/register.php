3470b91e0055695b496dc93216944586f995138c
hieunugent <hieunugent@gmail.com>
2021-10-09 21:36:48 -0700

    update register

---
+++ Blog/register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="views/login.css">
+    
+</head>
+<body class="page">
+    <div class="register_area">
+    <div class="register_page"> 
+        <h3>Sign In</h3>
+        <label class="label_register" for="Username"> User Name: </label>
+     
+        <input  type="text" placeholder="Username">
+  
+        <label class="label_register" for="Password">Password: </label>
+        <label class="label_register" for="Password">Retype Password: </label>
+        <input type="password" placeholder="Password">
+        <p> Or <a class="a-link"href="resigter.php" >Sign In</a> a User</p>
+    </div>
+
+    </div>
+    
+   
+
+</body>
+</html>
