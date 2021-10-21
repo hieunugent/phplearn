44891aa195850002d6891f989cf2f4d2024246c9
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-10-21 15:07:55 -0700

    update new branch

--- Blog/login.php
+++ Blog/login.php
@@ -11,9 +11,14 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
 
     if ($_POST['username']!= ''){
         $pwd_pepper2 = hash_hmac('sha256', $_POST['password'],"c1isvFdxMDdmjKOlvxpecFw");
-        $user = $users->findOne(['username'=>$_POST['username']]);
-        echo $user;
-        echo $db;
+        $user = $db->findOne(['username'=>$_POST['username']]);
+        if ($user){
+            echo (String)$user->password;
+        }else{
+            echo "notfound any";
+        }
+     
+      
     }
 
 }
@@ -34,25 +39,18 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
 </head>
 <body class="page">
     <div class="loginarea">
-        <form action="login.php">
+    <form action="login.php" method="post">
     <div class="loginpage"> 
         <h3>Sign In</h3>
-        
-
         <label class="labellogin" for="Username"> User Name: </label>
-     
-        <input  type="text" placeholder="Username">
-  
+        <input  type="text" name="username" placeholder="Username">
         <label class="labellogin" for="Password">Password: </label>
-      
-        <input type="password" placeholder="Password">
-        <input type="submit" name="signIn">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit" name="submit">
 
-   
-        
         <p> Or <a class="a-link"href="register.php" >Sign Up</a> a User</p>
     </div>
-     </form>
+    </form>
     </div>
     
    
