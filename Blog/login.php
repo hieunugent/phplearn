e64090bbeb9dbd08a5fe09f3689b5c2bd9c3746a
hieunugent <hieunugent@gmail.com>
2021-10-21 20:17:59 -0700

    merger 1.login to main

--- Blog/login.php
+++ Blog/login.php
@@ -12,10 +12,7 @@ if (isset($_POST['submit'])){
     if ($_POST['username']!= ''){
         $pwd_pepper2 = hash_hmac('sha256', $_POST['password'],"c1isvFdxMDdmjKOlvxpecFw");
         $user = $db->findOne(['username'=>$_POST['username']]);
-<<<<<<< HEAD
-     
-        echo $user->username;
-=======
+
         if ($user){
             echo (String)$user->password;
         }else{
@@ -23,7 +20,7 @@ if (isset($_POST['submit'])){
         }
      
       
->>>>>>> origin/1.Loginpage
+
     }
 
 }
@@ -47,7 +44,7 @@ if (isset($_POST['submit'])){
     <form action="login.php" method="post">
     <div class="loginpage"> 
         <h3>Sign In</h3>
-<<<<<<< HEAD
+
         
         <form action="login.php" method="post">
         <label class="labellogin" for="Username"> User Name: </label>
@@ -63,14 +60,8 @@ if (isset($_POST['submit'])){
         
    
         
-=======
-        <label class="labellogin" for="Username"> User Name: </label>
-        <input  type="text" name="username" placeholder="Username">
-        <label class="labellogin" for="Password">Password: </label>
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" name="submit">
 
->>>>>>> origin/1.Loginpage
+ 
         <p> Or <a class="a-link"href="register.php" >Sign Up</a> a User</p>
     </div>
     </form>
